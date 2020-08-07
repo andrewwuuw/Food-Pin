dbb3b95479ae55dc9534cf0d1acd40f0b0ceb405
chiaqingwu <secret8419@gmail.com>
2020-08-07 23:27:09 +0800

    feat(map): set the coordinate into map cell

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -78,6 +78,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailMapCell.self),
                                                      for: indexPath) as! RestaurantDetailMapCell
             
+            cell.configure(location: restaurant.location)
             cell.selectionStyle = .none
             return cell
         default:
--- FoodPin/View/RestaurantDetailMapCell.swift
+++ FoodPin/View/RestaurantDetailMapCell.swift
@@ -23,5 +23,33 @@ class RestaurantDetailMapCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func configure(location: String) {
+        // 取得位置
+        let geoCoder = CLGeocoder()
+        print("* \(location)")
+        
+        geoCoder.geocodeAddressString(location, completionHandler: { (placeMarks, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            // 加上標記
+            let annotation = MKPointAnnotation()
+            guard let placeMarks = placeMarks, let location = placeMarks[0].location else { return }
+            
+            // 顯示標記
+            annotation.coordinate = location.coordinate
+            self.mapView.addAnnotation(annotation)
+            
+            // 縮放程度
+            let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 250, longitudinalMeters: 250)
+            self.mapView.setRegion(region, animated: false)
+            
+            
+            
+        })
+    }
 
 }
